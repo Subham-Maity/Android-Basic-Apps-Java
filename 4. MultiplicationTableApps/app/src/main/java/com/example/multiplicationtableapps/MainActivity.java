201ee6e781a7f9607aff26bcae1162cab5e59913
Subham Maity <codexam.personal@gmail.com>
2022-02-24 09:24:57 +0530

    Adding Table Project 4

---
+++ 4. MultiplicationTableApps/app/src/main/java/com/example/multiplicationtableapps/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.multiplicationtableapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ListView listView;
+    SeekBar seekbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        seekbar = findViewById(R.id.seekBar);
+
+        seekbar.setMax(20);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(MainActivity.this, "Populating table of " + progress, Toast.LENGTH_SHORT).show();
+                populate(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void populate(int table){
+
+        ArrayList<String> mulTable  = new ArrayList<>();
+        for (int i =1; i<=10;i++){
+            mulTable.add(table + " X " + i + " = " + table*i);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mulTable);
+        listView.setAdapter(arrayAdapter);
+        textView.setText("Multiplication Table of " + table);
+
+
+    }
+}
